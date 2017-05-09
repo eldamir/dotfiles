37ded47b06dbfcafeeec00e885f6783531ac9854
Ruben Nielsen <nielsen.ruben@gmail.com>
2017-05-09 13:40:56 +0200

    Add ctags config to the repo
    Currently holds definitions for typescript *.tsx tags

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 # list of files/folders to symlink in homedir
-files="bashrc vimrc zshrc vim oh-my-zsh pylintrc gitconfig spacemacs"
+files="bashrc vimrc zshrc vim oh-my-zsh pylintrc gitconfig spacemacs ctags"
 dirs=".i3"
 
 ##########
